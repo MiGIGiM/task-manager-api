d5abd4d6b565f390a204ecf9765604765fe87449
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-08-08 21:05:38 -0600

    feat: adding user and task models

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age cannot be negative')
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid email')
+      }
+    }
+  },
+  password: {
+    type: String,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) throw new Error('Password cannot contain "password".')
+    }
+  }
+});
+
+module.exports = User
