525f8e18beb21f5b7278252296acff6e8422c137
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-08-09 22:33:17 -0600

    feat: added auth to patch and delete EPs

--- src/routers/user.js
+++ src/routers/user.js
@@ -59,50 +59,29 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) return res.status(400).send({ error: 'User not found' })
-
-    res.status(200).send(user)
-  } catch (e) {
-    res.status(400).send({ error: e.message })
-  }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
   try {
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    const user = await User.findById(req.params.id)
+    updates.forEach((update) => req.user[update] = req.body[update])
 
-    updates.forEach((update) => user[update] = req.body[update])
-
-    await user.save()
+    await req.user.save()
 
     if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates' })
 
-    if (!user) return res.status(404).send({ error: 'No user found' })
-
-    res.status(200).send(user)
+    res.status(200).send(req.user)
   } catch (e) {
     res.status(400).send({ error: e.message })
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
+    await req.user.remove()
 
-    if (!user) return res.status(404).send({ error: 'No user found' })
-
-    res.send(user)
+    res.send(req.user)
   } catch (e) {
     res.status(400).send({ error: e.message })
   }
